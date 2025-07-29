e5687e509b78b54e4e47f04dbfef8181305042d4
Saim20 <saim.29@outlook.com>
2025-07-29 14:52:58 +0600

    Website icon changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import AuthProvider from "@/lib/auth/auth-provider";
 import { bloxat } from "@/components/fonts";
 import Picker from "@/layouts/Picker";
-import { Toaster } from 'sonner';
+import { Toaster } from "sonner";
 
 export const metadata = {
   title: "Tathirmichil",
@@ -11,7 +11,9 @@ export const metadata = {
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" data-theme="light">
-      <body className={`${bloxat.className} bg-tathir-cream min-h-screen h-full w-full overflow-x-hidden`}>
+      <body
+        className={`${bloxat.className} bg-tathir-cream min-h-screen h-full w-full overflow-x-hidden`}
+      >
         <AuthProvider>
           <Picker>{children}</Picker>
         </AuthProvider>
